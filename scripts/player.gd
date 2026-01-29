cc96b3c4b489f107e0e83b70a4bec6b6e04187ac
tim2go <63936876+tim2go@users.noreply.github.com>
2026-01-29 15:49:51 -0800

    added stamina bar
    stamina drains when expanding petals

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,17 @@ extends CharacterBody2D
 
 var left_click_down : bool = false
 
+const max_stamina : int = 150
+const stamina_drain_per_sec : int = 75
+const stamina_recover_per_sec : int = 15
+var stamina : float = max_stamina
+
+@onready var stamina_bar_bg = %StaminaBarBg
+@onready var stamina_bar = %StaminaBar
+
 func _ready() -> void:
+	stamina_bar_bg.size.x = max_stamina
+	stamina_bar.size.x = stamina
 	for i in range(petal_num):
 		var new_pellet = pellet.instantiate() # creates new pellet object
 		new_pellet.current_angle = (360/petal_num) * i # calculates starting angle
@@ -29,12 +39,21 @@ func _physics_process(delta: float) -> void:
 		left_click_down = false
 	
 	# when left click held, radius of pellets increases
-	if left_click_down:
+	if left_click_down and stamina >= 0:
+		stamina -= stamina_drain_per_sec * delta
 		for p in Global.pellet_inventory:
 			p.dist = 200
 	else:
+		stamina += stamina_recover_per_sec * delta
 		for p in Global.pellet_inventory:
 			p.dist = 128
+			
+	if stamina > max_stamina:
+		stamina = max_stamina
+	if stamina < 0:
+		stamina = 0
+	
+	stamina_bar.size.x = stamina
 
 
 func _on_player_area_area_entered(area: Area2D) -> void:
