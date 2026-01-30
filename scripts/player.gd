f8f093311506ad969ea215c92faf377be2ea5b03
tim2go <63936876+tim2go@users.noreply.github.com>
2026-01-29 21:59:21 -0800

    fixed jitter
    lerped velocity when mouse is near player and made camera lag dropoff near the player faster

--- scripts/player.gd
+++ scripts/player.gd
@@ -38,8 +38,9 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2(speed * cos(move_angle), speed * sin(move_angle))
 		move_and_slide()
 	elif $".".position.distance_to(mouse_pos) > 5:
-		%Camera2D.position_smoothing_speed = 3 * ($".".position.distance_to(mouse_pos)/64)
+		%Camera2D.position_smoothing_speed = 3 * ($".".position.distance_to(mouse_pos)/30)
 		velocity = Vector2(speed * cos(move_angle), speed * sin(move_angle))
+		velocity = velocity.lerp(Vector2.ZERO, 0.6)
 		move_and_slide()
 	
 		
