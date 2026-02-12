09ceffc596fcd4f438874cf04ebde9fe7788e9c3
tim2go <63936876+tim2go@users.noreply.github.com>
2026-02-12 15:41:27 -0800

    added pathfinding to enemys
    jay goon can now hunt you down

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -17,8 +17,14 @@ var bounce_back_time_speed : float = 150
 var bounce_back_timer : float = -1
 var bounce_back_speed : int = speed * -0.8
 
+@onready var nav_agent : NavigationAgent2D = $NavigationAgent2D as NavigationAgent2D
+#j
+func _ready() -> void:
+	makepath()
+
 func _physics_process(delta: float) -> void:
 	if not Global.paused:
+		#bounce back code
 		if bounce_back_timer > -0.5:
 			bounce_back_timer += bounce_back_time_speed * delta * 2
 			speed = lerp(speed, bounce_back_speed, delta * 10)
@@ -28,10 +34,9 @@ func _physics_process(delta: float) -> void:
 			bounce_back_timer = -1
 		
 		# movement code
-		move_angle = position.angle_to_point(player.position)
-		if $".".position.distance_to(player.position) > 5:
-			velocity = Vector2(speed * cos(move_angle), speed * sin(move_angle))
-			move_and_slide()
+		var dir = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = dir * speed
+		move_and_slide()
 			
 		if health <= 0:
 			queue_free()
@@ -40,9 +45,16 @@ func _physics_process(delta: float) -> void:
 			health = max_health
 		hp_bar.size.x = (hp_bar_bg.size.x/max_health) * health
 
+func makepath() -> void:
+	nav_agent.target_position = player.global_position
+
 # called when enemy collides with an area2d node
 func _on_enemy_area_area_entered(area: Area2D) -> void:
 	if area.name == "PelletArea": # if area2d is a pellet
 		Global.collided_with_pellet.emit(area) # emit signal to pellet
 		health -= area.get_parent().damage
 		bounce_back_timer = 0
+
+
+func _on_timer_timeout() -> void:
+	makepath()
